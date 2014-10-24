6969c69920d316aa0bebac12fea6d1c1fb991f8a
Imre Deak <imre.deak@intel.com>
2014-10-24 20:29:10 +0300

    PM / Sleep: fix recovery during resuming from hibernation
    If a device's dev_pm_ops::freeze callback fails during the QUIESCE
    phase, we don't rollback things correctly calling the thaw and complete
    callbacks. This could leave some devices in a suspended state in case of
    an error during resuming from hibernation.
    
    Change-Id: Iffaa06a75de8bdd672c7f54384cb44e5df1e73e8
    Signed-off-by: Imre Deak <imre.deak@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/power/wakeup_reason.c
+++ kernel/power/wakeup_reason.c
@@ -36,8 +36,6 @@ static spinlock_t resume_reason_lock;
 
 static ssize_t last_resume_reason_show(struct kobject *kobj, struct kobj_attribute *attr,
 		char *buf)
-static ssize_t reason_show(struct kobject *kobj, struct kobj_attribute *attr,
-		char *buf)
 {
 	int irq_no, buf_offset = 0;
 	struct irq_desc *desc;
@@ -56,8 +54,6 @@ static ssize_t reason_show(struct kobject *kobj, struct kobj_attribute *attr,
 }
 
 static struct kobj_attribute resume_reason = __ATTR_RO(last_resume_reason);
-static struct kobj_attribute resume_reason = __ATTR(last_resume_reason, 0666,
-		reason_show, NULL);
 
 static struct attribute *attrs[] = {
 	&resume_reason.attr,
@@ -117,7 +113,6 @@ static struct notifier_block wakeup_reason_pm_notifier_block = {
  * registers the pm_event notifier
  */
 int __init wakeup_reason_init(void)
-void __init wakeup_reason_init(void)
 {
 	int retval;
 	spin_lock_init(&resume_reason_lock);
@@ -131,7 +126,6 @@ void __init wakeup_reason_init(void)
 		printk(KERN_WARNING "[%s] failed to create a sysfs kobject\n",
 				__func__);
 		return 1;
-		return;
 	}
 	retval = sysfs_create_group(wakeup_reason, &attr_group);
 	if (retval) {
