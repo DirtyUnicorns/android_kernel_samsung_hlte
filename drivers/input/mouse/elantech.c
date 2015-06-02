86ee9c87ec47d1eb1a13e6dddc49cb6bd37b10a7
Hans de Goede <hdegoede@redhat.com>
2015-06-02 10:40:50 -0700

    Input: elantech - fix detection of touchpads where the revision matches a known rate
    commit 5f0ee9d17aae628b22be86966471db65be21f262 upstream.
    
    Make the check to skip the rate check more lax, so that it applies
    to all hw_version 4 models.
    
    This fixes the touchpad not being detected properly on Asus PU551LA
    laptops.
    
    Reported-and-tested-by: David Zafra Gómez <dezeta@klo.es>
    Signed-off-by: Hans de Goede <hdegoede@redhat.com>
    Signed-off-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Signed-off-by: Zefan Li <lizefan@huawei.com>
    
    Conflicts:
    	drivers/input/mouse/elantech.c
    
    Change-Id: Ic40402d6adf9fe1b8ca8aafe39f12bc7186acb67

--- drivers/input/mouse/elantech.c
+++ drivers/input/mouse/elantech.c
@@ -1220,6 +1220,14 @@ static bool elantech_is_signature_valid(const unsigned char *param)
 	if (param[1] == 0)
 		return true;
 
+	/*
+	 * Some hw_version >= 4 models have a revision higher then 20. Meaning
+	 * that param[2] may be 10 or 20, skip the rates check for these.
+	 */
+	if ((param[0] & 0x0f) >= 0x06 && (param[1] & 0xaf) == 0x0f &&
+	    param[2] < 40)
+		return true;
+
 	for (i = 0; i < ARRAY_SIZE(rates); i++)
 		if (param[2] == rates[i])
 			return false;
